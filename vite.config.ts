b28a7359117d6094efeb8583887cb577619faf80
jinxu <J634167397@163.com>
2022-08-05 15:53:48 +0800

    chore: config add baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,4 +21,9 @@ export default defineConfig({
     port: 3000,
   },
   base: "/website/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
 });
