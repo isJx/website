673d80a740ba90e664a39ab2a5e6b75585e44a8c
jinxu <J634167397@163.com>
2022-09-05 22:30:25 +0800

    feat: menu add dropdown

--- src/layout/NavBar.vue
+++ src/layout/NavBar.vue
@@ -1,23 +1,70 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    router
+  <div
+    style="
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    "
   >
-    <MenuItem :baseRouter="baseRouter" />
-  </el-menu>
+    <div>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        router
+      >
+        <MenuItem :baseRouter="baseRouter" />
+      </el-menu>
+    </div>
+    <div>
+      <el-dropdown @command="handleClick">
+        <span class="el-dropdown-link">
+          {{ userInfo.userName }}
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="logout">注销</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import MenuItem from "@/components/MenuItem.vue";
 import { baseRouter, routePath } from "@/router/index";
-import { ref, unref, watchEffect } from "vue";
+import { onMounted, ref, unref, watchEffect } from "vue";
 
 const activeIndex = ref<string>("");
 
+type UserInfo = {
+  userName?: string;
+  passWord?: string;
+};
+
+const userInfo = ref<UserInfo>({});
+
 watchEffect(() => {
   activeIndex.value = unref(routePath);
+});
+
+const handleClick = (val: string) => {
+  console.log(11, val);
+};
+
+const getUserInfo = () => {
+  userInfo.value = JSON.parse(
+    window.localStorage.getItem("userInfo") as string
+  );
+};
+
+onMounted(() => {
+  getUserInfo();
 });
 </script>
 
